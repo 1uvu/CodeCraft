63921bba35aac045ccd9925fc9518774c1c4735e
1uvu <zjh98@vip.qq.com>
2021-04-25 16:22:22 +0800

    add tmpl

---
+++ leetcode/lc424/craft.go
@@ -0,0 +1,15 @@
+/* -*- encoding: utf-8 -*- */
+/*
+@File    :   craft.go
+@URL     :   ""
+@Tags    :   []
+---------------------------
+@Idea:
+ - 
+ - 
+*/
+package leetcode
+
+func Foo(i int) int {
+	return i
+}
---
+++ leetcode/lc424/craft_test.go
@@ -0,0 +1,30 @@
+/* -*- encoding: utf-8 -*- */
+/*
+@File    :   craft_test.go
+*/
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type SingleTest struct {
+	in  interface{}
+	exp interface{}
+}
+
+func Test(t *testing.T) { // rename function
+	tests := []SingleTest{
+		{1, 1},
+		{2, 2},
+		// ...
+	}
+	fmt.Println("begin testing...")
+	for _, _t := range tests {
+		_res := Foo(_t.in.(int)) // change there `in` type
+		if _res != _t.exp.(int) {
+			t.Error(_t.in, _res, _t.exp)
+		}
+	}
+}
